99bb643217b86ee785a067f2cad3fdd2e9926e6d
Mirco Kolarczik <mirco@swabianinstruments.com>
2020-11-18 11:02:47 +0100

    Status before implementing clock PLL

---
+++ PPS_GUI.py
@@ -0,0 +1,301 @@
+import tkinter as tk
+from tkinter import BooleanVar, IntVar, StringVar, ttk, messagebox
+from threading import Thread
+from datetime import time
+from os.path import dirname, realpath, exists
+import pickle
+from time import sleep
+from tkinter.constants import LEFT
+from typing import Optional, Dict, Union, List, Callable
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+from matplotlib.figure import Figure
+import TimeTagger
+from enum import Enum
+from PPStracking import PpsTracking
+
+
+DIVIDER = 10 # just used for testing with Pulse Streamer, should otherwise always be 1
+
+def this_folder():
+    return dirname(realpath(__file__))
+
+
+class ChannelRoles(Enum):
+    UNUSED = "Unused"
+    REFERENCE = "Use as Reference"
+    CHANNEL = "Signal channel"
+    CLOCK = "10 MHz clock"
+
+
+class DisplayUpdater(Thread):
+    def __init__(self, measurement: PpsTracking, plot: Figure, channels: List[int], add_message: Callable[[str], None]):
+        super().__init__()
+        self.measurement = measurement
+        self.plot = plot
+        self.last_tag = -1
+        self.last_message = 0
+        self.channels = channels
+        self.channel_names = measurement.getChannelNames()
+        self.add_message = add_message
+    
+    def run(self):
+        while self.measurement.isRunning():
+            timetag_number, message_number = self.measurement.getMeasurementStatus()
+            if timetag_number > self.last_tag:
+                data = self.measurement.getData()
+                index = self.measurement.getIndex()
+                for ch in range(data.shape[0]):
+                    axis = self.plot.get_axes()[ch]
+                    axis.clear()
+                    axis.set_ylabel(f"{self.channel_names[ch]}\nOffset, ps")
+                    axis.scatter(index, data[ch, :])
+                axis.set_xlabel("Time tag index")
+                self.plot.canvas.draw_idle()
+                self.last_tag = timetag_number
+            if message_number > self.last_message:
+                for msg in self.measurement.getMessages(self.last_message):
+                    self.add_message(msg)
+                self.last_message = message_number
+            sleep(0.1)
+
+class PPS_App:
+    """The graphical user interface for tracking PPS signals."""
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.iconbitmap(this_folder() + "/iconTT.ico")
+        self.fig = Figure()
+        self.measurement: Optional[PpsTracking] = None
+        self.tagger: Optional[TimeTagger.TimeTagger] = None
+        taggers = TimeTagger.scanTimeTagger()
+        self.settings: Dict[str, Union[StringVar, dict]] = dict(serial=tk.StringVar(self.root, taggers[0] if taggers else ""),
+                                                                channels={ch: tk.StringVar(self.root, ChannelRoles.UNUSED.value) for ch in range(1,9)},
+                                                                channel_names={ch: tk.StringVar(self.root, "") for ch in range(1,9)},
+                                                                storage_folder=StringVar(self.root, ""),
+                                                                store_debug_info=BooleanVar(self.root, False),
+                                                                storage_time={key: StringVar(self.root, "00", key) for key in ("hour", "minute", "second")},
+                                                                clock_divider=IntVar(self.root, 1))
+        self._load_settings()
+        for var in self.settings["storage_time"].values():
+            var.trace("w", self._adjust_storage_time)
+        self.settings["storage_folder"].trace("w", self._adjust_storage_folder)
+        panes = tk.PanedWindow(orient=tk.HORIZONTAL)
+        panes.pack(fill=tk.BOTH, expand=1)
+        self.messages = tk.Listbox(panes)
+        panes.add(self.messages)
+        self.fig_frame = tk.Frame(panes)
+        panes.add(self.fig_frame)
+        self.root.protocol("WM_DELETE_WINDOW", self._quit)
+        self.root.state("zoomed")
+        
+    def run(self):
+        """Run the tkinter application."""
+        self.root.wm_title("PPS tracking")
+        self._setup_menu()
+        self._setup_figure()
+        tk.mainloop()
+
+    def _setup_menu(self):
+        """Setup of the tkinter menus."""
+        menu = tk.Menu(self.root)
+        self.root.config(menu=menu)
+        file_menu = tk.Menu(menu, tearoff=0)
+        menu.add_cascade(label="File", menu=file_menu)
+        file_menu.add_command(label="Hardware settings", command=lambda: SettingsWindow(self, self.settings))
+        file_menu.add_command(label="Storage settings", command=lambda: StorageConfigWindow(self, self.settings))
+        file_menu.add_command(label="Close", command=self._quit)
+        meas_menu = tk.Menu(menu, tearoff=0)
+        menu.add_cascade(label="Measurement", menu=meas_menu)
+        meas_menu.add_command(label="Start measurement", command=self._start_measurement)
+        meas_menu.add_command(label="Stop measurement", command=self._stop_measurement)
+
+    def _setup_figure(self):
+        """Prepare graph area for pyplot."""
+        canvas = FigureCanvasTkAgg(self.fig, master=self.fig_frame)
+        canvas.draw()
+        canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+        toolbar = NavigationToolbar2Tk(canvas, self.fig_frame)
+        toolbar.update()
+        canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+
+    def _adjust_storage_time(self, *args):
+        if self.measurement:
+            self.measurement.setNewFileTime(time(**{key: int(self.settings["storage_time"][key].get()) for key in ["hour", "minute", "second"]}))
+
+    def _adjust_storage_folder(self, *args):
+        if self.measurement:
+            self.measurement.setFolder(self.settings["storage_folder"].get())
+
+    def _start_measurement(self):
+        if not exists(self.settings["storage_folder"].get()):
+            self.add_message("Data folder does not exist")
+            return
+        clock = None
+        reference_name = ""
+        reference = 0
+        for ch, val in self.settings["channels"].items():
+            if val.get() == ChannelRoles.REFERENCE.value:
+                reference = ch
+                reference_name = self.settings["channel_names"][ch].get()
+            elif val.get() == ChannelRoles.CLOCK.value:
+                clock = ch
+        if not reference:
+            self.add_message("No reference given")
+            return
+
+        try:
+            self.tagger = TimeTagger.createTimeTagger(self.settings["serial"].get())
+        except RuntimeError:
+            serial = self.settings["serial"].get()
+            self.add_message(f"Cannot connect to Time Tagger '{serial}'")
+            return
+        self.tagger.reset()
+        channels = list()
+        channel_names = list()
+        for channel, value, name in zip(self.settings["channels"].keys(), self.settings["channels"].values(), self.settings["channel_names"].values()):
+            if value.get() == ChannelRoles.CHANNEL.value:
+                channels.append(channel)
+                channel_names.append(name.get())
+                self.tagger.setEventDivider(channel, DIVIDER)
+        self.tagger.setEventDivider(reference, DIVIDER)
+        if clock:
+            self.tagger.setEventDivider(clock, self.settings["clock_divider"].get())
+        self.measurement = PpsTracking(self.tagger,
+                                       channels=channels,
+                                       reference=reference,
+                                       clock=clock,
+                                       clock_period=100000*self.settings["clock_divider"].get(),
+                                       channel_names=channel_names,
+                                       debug_to_file=self.settings["store_debug_info"].get(),
+                                       reference_name=reference_name,
+                                       folder=self.settings["storage_folder"].get())
+        self._adjust_storage_time()
+        self.fig.clear()
+        self.fig.subplots(len(channels), 1, sharex="all")
+        self.fig.subplots_adjust(hspace=0, right=0.99, top=0.99, bottom=0.1, left=0.1)
+        self.fig.canvas.draw_idle()
+        updater = DisplayUpdater(self.measurement, self.fig, channels, self.add_message)
+        updater.start()
+
+    def _stop_measurement(self):
+        try:
+            self.measurement.stop()
+            TimeTagger.freeTimeTagger(self.tagger)
+            self.measurement = None
+            self.tagger = None
+        except AttributeError:
+            pass
+
+    def add_message(self, msg):
+        self.messages.insert(0, msg)
+
+    def _quit(self):
+        if not messagebox.askyesno("Quit?", "Do you really want to close the program?"):
+            return
+        self._stop_measurement()
+        self._save_settings()
+        self.root.quit()
+        self.root.destroy()
+    
+    def _save_settings(self):
+        def to_string_dict(original: dict):
+            new = dict()
+            for key, value in original.items():
+                if isinstance(value, tk.Variable):
+                    new[key] = value.get()
+                elif isinstance(value, dict):
+                    new[key] = to_string_dict(value)
+            return new
+        with open("settings.dat", "wb") as storage:
+            pickle.dump(to_string_dict(self.settings), storage)
+
+    def _load_settings(self):
+        def from_string_dict(settings: dict, dump: dict):
+            for key in settings:
+                if key in dump:
+                    if isinstance(dump[key], dict):
+                        from_string_dict(settings[key], dump[key])
+                    else:
+                        settings[key].set(dump[key])
+        try:
+            with open("settings.dat", "rb") as storage:
+                from_string_dict(self.settings, pickle.load(storage))
+        except:
+            pass
+        
+
+class ModalWindow(tk.Toplevel):
+    def __init__(self, parent: PPS_App, title: str):
+        super().__init__()
+        self.iconbitmap(this_folder() + "/iconTT.ico")
+        self.parent = parent.root
+        self.title(title)
+        self.grab_set()
+        self.focus()
+        self.protocol("WM_DELETE_WINDOW", self.cancel)
+
+    def cancel(self, event=None):
+        self.parent.focus_set()
+        self.destroy()
+
+
+class SettingsWindow(ModalWindow):
+    def __init__(self, parent: PPS_App, settings: Dict[str, StringVar]):
+        super().__init__(parent=parent, title="Settings")
+        ttk.Label(self, text="Serial number").grid(row=0, column=0, pady=10, sticky=tk.E)
+        taggers = TimeTagger.scanTimeTagger()
+        tt_select = ttk.OptionMenu(self, settings["serial"], taggers[0] if taggers else None, *taggers)
+        tt_select.grid(row=0, column=1)
+
+        ttk.Label(self, text="Role").grid(row=1, column=1)
+        ttk.Label(self, text="Name").grid(row=1, column=2)
+        for i in range(1, 9):
+            ttk.Label(self, text=f"Input {i}").grid(row=i+1, column=0, sticky=tk.E)
+            ttk.OptionMenu(self, settings["channels"][i], settings["channels"][i].get(), *[item.value for item in ChannelRoles]).grid(row=i+1, column=1)
+            ttk.Entry(self, textvariable=settings["channel_names"][i]).grid(row=i+1, column=2)
+
+        ttk.Label(self, text="Clock divider").grid(row=10, column=0, sticky=tk.E, pady=10)
+        tk.Spinbox(self, textvariable=settings["clock_divider"], from_=1, to=10000, width=5, state="readonly").grid(row=10, column=1)
+
+        ttk.Label(self, text="Store debug information").grid(row=11, column=0, sticky=tk.E, pady=10)
+        tk.Checkbutton(self, variable=settings["store_debug_info"]).grid(row=11, column=1)
+
+
+class StorageConfigWindow(ModalWindow):
+    def __init__(self, parent: PPS_App, settings: dict):
+        super().__init__(parent=parent, title="Storage configuration")
+        tk.Grid.columnconfigure(self, 1, weight=1)
+        browser = tk.Frame(self)
+        browser.grid(row=0, column=1, sticky=tk.E+tk.W)
+        self.filename = settings["storage_folder"]
+        tk.Label(self, text="Data folder").grid(row=0, column=0, sticky=tk.E)
+        tk.Entry(browser, textvariable=self.filename, state="disabled").pack(side=tk.LEFT, fill=tk.X, expand=1)
+        tk.Button(browser, text="Browse", command=self.browse_folder).pack(side=tk.RIGHT)
+        tk.Label(self, text="New file time").grid(row=1, column=0, sticky=tk.E)
+        self.time_display = tk.Frame(self)
+        self.time_display.grid(row=1, column=1, sticky=tk.W)
+        self._time_digit(settings["storage_time"], "hour")
+        tk.Label(self.time_display, text=":").pack(side=tk.LEFT)
+        self._time_digit(settings["storage_time"], "minute")
+        tk.Label(self.time_display, text=":").pack(side=tk.LEFT)
+        self._time_digit(settings["storage_time"], "second")
+
+    def _time_digit(self, settings: dict, key: str):
+        max_value = dict(hour=23, minute=59, second=59)
+        tk.Spinbox(self.time_display,
+                   textvariable=settings[key],
+                   state="readonly",
+                   width=2,
+                   from_=0,
+                   to=max_value[key],
+                   format_="%02.0f").pack(side=tk.LEFT)
+
+    def browse_folder(self):
+        self.filename.set(tk.filedialog.askdirectory())
+
+    def run(self):
+        self.withdraw()
+
+
+if __name__ == "__main__":
+    window = PPS_App()
+    window.run()
---
+++ PPStracking.py
@@ -0,0 +1,278 @@
+from io import TextIOWrapper
+from typing import List, Dict, Optional, Tuple
+import numpy
+from numba import jit, jitclass
+import csv
+from os.path import isdir
+from os import getcwd
+from datetime import datetime, timedelta, timezone, time
+import TimeTagger
+
+NO_SIGNAL_THRESHOLD = timedelta(seconds=5)
+
+class TimeTagGroup:
+    def __init__(self, index, reference_tag: int, clock_offset: int, time: datetime, debug_data: List[str]):
+        self.reference_tag = reference_tag
+        self.channel_tags = dict()
+        self.time = time
+        self.index = index
+        self.clock_offset = clock_offset
+        self.debug_data = debug_data
+
+    def add_tag(self, channel: int, timetag: int):
+        self.channel_tags[channel] = timetag
+
+    def get_reference_with_offset(self):
+        return self.reference_tag - self.clock_offset 
+
+    def get_missing_channels(self, channels: List[int]):
+        missing = list(channels)
+        try:
+            for channel in self.channel_tags:
+                missing.remove(channel)
+        except ValueError:
+            pass
+        return missing
+
+    def get_channel_tags(self, channels: List[int]):
+        return [self.channel_tags[channel] - self.reference_tag if channel in self.channel_tags else numpy.nan for channel in channels]
+
+
+class MissingTimeTagGroups:
+    def __init__(self, periods: int, unused_tags):
+        self.periods = periods
+        self.unused_tags = unused_tags
+
+
+class Clock:
+    """Information on the external clock."""
+    def __init__(self, channel: int, period: int):
+        self.channel = channel
+        self.period = period
+        self.last_time_tag = 0
+        self._offset = 0
+
+    def update_offset(self, elapsed_time: int, periods: int):
+        self._offset += elapsed_time - periods * self.period
+
+    def get_offset(self):
+        return self._offset
+
+
+class PpsTracking(TimeTagger.CustomMeasurement):
+    def __init__(self,
+                 tagger: TimeTagger.TimeTaggerBase,
+                 channels: List[int],
+                 reference: int,
+                 clock: Optional[int] = None,
+                 clock_period: int = 100000,
+                 period: int = 1000000000000,
+                 folder: str = None,
+                 channel_names: Optional[List[str]] = None,
+                 reference_name: str = "Reference",
+                 debug_to_file: bool = False):
+        super().__init__(tagger)
+        self.data_file: Optional[TextIOWrapper] = None
+        self._current_index = 0
+        self._channel_tags = list()
+        self._reference_tag = None
+        self._last_signal_time = self._now()
+        self._last_reference_time = None
+        self._last_time_check = self._now()
+        self._messages = list()
+        self._timetags = list()
+        self._max_timetags = 300
+        self._timetag_index = 0
+        self._message_index = 0
+        self._last_clock_tag = 0
+        self._number_of_clock_cycles = 0
+        self._ps_last_ref_after_clock = 0
+        # self._clock_offset = 0
+
+        self.channels = channels
+        if clock:
+            self.clock = Clock(clock, clock_period)
+        self.period = period
+        # self.clock_period = clock_period
+        self.channel_names = []
+        self.reference_name = reference_name
+        for channel, name in zip(self.channels, channel_names if channel_names else [""] * len(channels)):
+            self.channel_names.append(name if name else f"Channel {channel}")
+        self.reference = reference
+        if folder is None or not isdir(folder):
+            self.folder = getcwd()
+        else:
+            self.folder = folder
+        self.new_file_time = time(hour=0, minute=0, second=0)
+        try:
+            if tagger.getModel() == "Time Tagger 20" and debug_to_file:
+                self._new_message("Time Tagger 20 cannot add debug data")
+                debug_to_file = False
+        except AttributeError:
+            debug_to_file = False
+        self.debug_to_file = debug_to_file
+        for channel in channels:
+            self.register_channel(channel)
+        self.register_channel(reference)
+        if clock:
+            self.register_channel(clock)
+        self._open_file(self._last_time_check)
+        self.clear_impl()
+        self.finalize_init()
+
+    def __del__(self):
+        self._close_file()
+        self.stop()
+
+    def getData(self):
+        data = numpy.empty(
+            [len(self.channels), len(self._timetags)], dtype=float)
+        for index, tag in enumerate(self._timetags):
+            data[:, index] = tag.get_channel_tags(self.channels)
+        return data
+
+    def getIndex(self):
+        return numpy.arange(self._timetag_index - len(self._timetags), self._timetag_index)
+
+    def getMeasurementStatus(self):
+        return self._timetag_index, self._message_index
+
+    def getChannelNames(self):
+        return self.channel_names
+
+    def measure(self, incoming_tags, begin_time, end_time):
+        """Method called by TimeTagger.CustomMeasurement for processing of incoming time tags."""
+        current_time = self._now()
+        tag_index = 0
+        while len(incoming_tags) > tag_index:
+            if self.clock:
+                periods, last_clock_tag = PpsTracking.remove_clock(incoming_tags[tag_index:], self.clock.channel)
+                if periods:
+                    if self._last_clock_tag:
+                        tag_index += periods
+                        self.clock.update_offset(last_clock_tag - self._last_clock_tag, periods)
+                    self._last_clock_tag = last_clock_tag
+                    if tag_index == len(incoming_tags):
+                        break
+            channel = incoming_tags[tag_index]["channel"]
+            timetag = incoming_tags[tag_index]["time"]
+            tag_index += 1
+            if channel == self.reference:
+                ps_this_ref_after_clock = timetag - self._last_clock_tag
+                if self._reference_tag is not None:
+                    index = self._select_tags_within_range(timetag)
+                    self._channel_tags = self._channel_tags[index:]
+                self._ps_last_ref_after_clock = ps_this_ref_after_clock
+                self._number_of_clock_cycles = 0
+                self._last_reference_time = current_time
+                self._next_tag_group(timetag, current_time)
+            else:
+                self._channel_tags.append((channel, timetag))
+            self._last_signal_time = current_time
+        if current_time - self._last_signal_time > NO_SIGNAL_THRESHOLD:
+            self._new_message(
+                "No incoming signals for more than " + str(NO_SIGNAL_THRESHOLD.seconds) + " s.", current_time)
+            self._last_signal_time = current_time
+
+    def _select_tags_within_range(self, this_timetag):
+        lower_limit = self._reference_tag.reference_tag - self.period//2
+        upper_limit = self._reference_tag.reference_tag + self.period//2
+        index = 0
+        for index, (channel, timetag) in enumerate(self._channel_tags):
+            if timetag > upper_limit:
+                break
+            if timetag >= lower_limit:
+                self._reference_tag.add_tag(channel, timetag)
+            else:
+                num_periods = (
+                    this_timetag - self._reference_tag.reference_tag - self.period//2) // self.period
+                if num_periods:
+                    MissingTimeTagGroups(
+                        num_periods, self._channel_tags[:index])
+        return index
+
+    def clear_impl(self):
+        pass
+
+    def setTimetagsMaximum(self, value: int):
+        self._max_timetags = value
+
+    def setFolder(self, folder):
+        self.folder = folder
+
+    def setNewFileTime(self, value: time):
+        self.new_file_time = value
+
+    def getMessages(self, from_index: int):
+        return self._messages[from_index:]
+
+    def _now(self):
+        return datetime.now(timezone.utc)
+
+    def _new_message(self, message: str, time: Optional[datetime] = None):
+        if time is None:
+            time = self._now()
+        message = f"{time.replace(microsecond=0).isoformat()}: {message}"
+        self._messages.append(message)
+        self._message_index += 1
+
+    def _next_tag_group(self, timetag: int, current_time: datetime):
+        if self._reference_tag is not None:
+            if missing := self._reference_tag.get_missing_channels(self.channels):
+                self._new_message(f"Tags missing: " +
+                                  ", ".join([f"input {ch}" for ch in missing]))
+            self._timetags.append(self._reference_tag)
+            self._store_timetag(self._reference_tag)
+            if len(self._timetags) > self._max_timetags:
+                self._timetags = self._timetags[-self._max_timetags:]
+        self._reference_tag = TimeTagGroup(
+            self._timetag_index, timetag, self.clock.get_offset(), current_time, self.get_sensor_data(1) if self.debug_to_file else [])
+        self._timetag_index += 1
+
+    def get_sensor_data(self, col: int) -> list:
+        return [row[col] for row in csv.reader(self.tagger.getSensorData().split("\n"), delimiter="\t") if row]
+
+    def _open_file(self, current_time: datetime):
+        self._close_file()
+        filename = self.folder + "/" + current_time.strftime("%Y-%m-%d_%H-%M-%S.csv")
+        self.data_file = open(filename, "w", newline="")
+        writer = csv.writer(self.data_file, delimiter=",")
+        debug_header = self.get_sensor_data(0) if self.debug_to_file else []
+        writer.writerow(["Index", "UTC", self.reference_name] +
+                        self.channel_names + debug_header)
+        self._new_message("New file opened: "+filename)
+
+    def _close_file(self):
+        if self.data_file:
+            filename = self.data_file.name
+            self.data_file.close()
+            self.data_file = None
+            self._new_message("File closed: "+filename)
+
+    def _store_timetag(self, tag: TimeTagGroup):
+        if self.data_file is None:
+            self._open_file(tag.time)
+        else:
+            if tag.time.day == self._last_time_check.day:
+                if self._last_time_check.time() < self.new_file_time <= tag.time.time():
+                    self._open_file(tag.time)
+            else:
+                if tag.time.time() >= self.new_file_time or self.new_file_time >= self._last_reference_time.time():
+                    self._open_file(tag.time)
+        writer = csv.writer(self.data_file, delimiter=",")
+        writer.writerow([tag.index, tag.time.replace(microsecond=0).isoformat(), tag.get_reference_with_offset()] +
+                        tag.get_channel_tags(self.channels) + tag.debug_data)
+        self._last_time_check = tag.time
+
+    @staticmethod
+    @jit(nopython=True, nogil=True)
+    def remove_clock(tags: numpy.array, clock_channel: int):
+        index = 0
+        last_clock = 0
+        for tag in tags:
+            if tag["channel"] == clock_channel:
+                last_clock = tag["time"]
+                index += 1
+                continue
+            break
+        return index, last_clock
